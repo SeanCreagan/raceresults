b9b02d027758de8199070ccbeec2d13d28798d15
Sean Creagan <screagan73@gmail.com>
2017-01-24 21:25:15 -0500

    Update connection.php

--- nensa_results-master/connection.php
+++ nensa_results-master/connection.php
@@ -2,8 +2,8 @@
 
 	$hostname = "localhost";
 	$username = "nensa";
-	$password = "Mas9EiwYjqzNBcqL";
-	$database = "nensa_results_2017";
+	$password = "";
+	$database = "dbnamehere";
 
 
 	$conn = mysqli_connect("$hostname","$username","$password","$database") or die(mysqli_error());
